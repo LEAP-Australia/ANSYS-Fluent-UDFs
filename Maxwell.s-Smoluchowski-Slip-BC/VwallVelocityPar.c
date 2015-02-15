68842466924f17ad95b4b2c625ab4ea9683bc321
Nish Joseph <nish.joseph@leapaust.com.au>
2015-02-16 09:30:35 +1100

    MAINT: Maxwell.s-Smoluchowski-Slip-BC: Added initial c files

--- Maxwell.s-Smoluchowski-Slip-BC/VwallVelocityPar.c
+++ Maxwell.s-Smoluchowski-Slip-BC/VwallVelocityPar.c
@@ -1,5 +1,45 @@
- #include "udf.h"
- #include <math.h>
+/*
+Copyright (c) 2014, LEAP Pty Ltd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or
+without modification, are permitted provided that the following
+conditions are met:
+
+1. Redistributions of source code must retain the above
+copyright notice, this list of conditions and the following
+disclaimer.
+
+2. Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials provided
+with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived
+from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Discription:
+Using DEFINE_PROFILE Macro to Implement Maxwell's Slip and Smoluchowski
+Temperature Jump for Density Based Solver
+
+This example a real values scheme variable named velocity-inlet is
+defined in scheme. This value is used to do define a profile.
+*/
+#include "udf.h"
+#include <math.h>
 
  DEFINE_PROFILE(V_Slip_Y_Profile, thread, position)
  {
